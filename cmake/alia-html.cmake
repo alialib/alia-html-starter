bf4eb397247e04c935f3a15b49112a9066148a45
Thomas Madden <tmadden@gmail.com>
2021-03-09 12:01:52 -0500

    Remove 'MODULARIZE' option

--- cmake/alia-html.cmake
+++ cmake/alia-html.cmake
@@ -10,8 +10,6 @@ FetchContent_MakeAvailable(alia-html)
 string(APPEND CMAKE_CXX_FLAGS
     " -s EXTRA_EXPORTED_RUNTIME_METHODS='[\"UTF8ToString\"]'")
 string(APPEND CMAKE_CXX_FLAGS
-    " -s MODULARIZE=1")
-string(APPEND CMAKE_CXX_FLAGS
     " -s ENVIRONMENT=web")
 string(APPEND CMAKE_CXX_FLAGS
     " -s WASM=1 --bind")
